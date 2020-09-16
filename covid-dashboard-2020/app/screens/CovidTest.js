216e9b5373028833ef550fea54ea5214b3c3b804
rschneid1 <55038099+rschneid1@users.noreply.github.com>
2020-09-15 21:36:50 -0400

    Add files via upload
    Page of buttons

--- covid-dashboard-2020/app/screens/CovidTest.js
+++ covid-dashboard-2020/app/screens/CovidTest.js
@@ -1,66 +1,167 @@
 import React from 'react';
 import { Image,
 		 Text,
-		 SectionList,
 		 StyleSheet,
+		 Button,
 		 View, 
-		 SafeAreaView
+		 SafeAreaView,
+		 Alert,
 	} from 'react-native';
 import Constants from "expo-constants";
 
-const QUESTIONS = [
-	{
-		title:"Q1",
-		data:["Yes","No"]
-	},
-	{
-		title:"Q2",
-		data:["Yes","No"]
-	},
-	{
-		title:"Q3",
-		data:["Yes","No"]
-	},
-];
-
-const Item = ({ title }) => (
-	<View style={styles.item}>
-		<Text style={styles.title}>{title}</Text>
-	</View>
+const Separator = () => (
+  <View style={styles.separator} />
 );
 
 const Test = () => (
 	<SafeAreaView style={styles.container}>
-		<SectionList
-			sections={QUESTIONS}
-			keyExtractor={(item,index) => item + index}
-			renderItem={({ item }) => <Item title={item} />}
-			renderSectionHeader={({ section: { title } }) => (
-				<Text style={styles.header}>{title}</Text>
-			)}
-		/>
+		<View>
+			<Text style={styles.title}>
+				Fever of 100 F, or feeling unusually hot (if no thermometer available) accompanied by shivering/chills
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={() => Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={() => Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				New cough not related to chronic condition
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				Difficulty breathing, Shortness of breath
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				Sore Throat
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				New loss of taste or smell
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				Vomiting
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				Severe fatigue
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
+		<Separator />
+		<View>
+			<Text style={styles.title}>
+				Severe muscle aches
+			</Text>
+			<View style={styles.fixToText}>
+				<Button
+					title="No"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+				<Button
+					title="Yes"
+					onPress={()=> Alert.alert('Simple Button pressed')}
+				/>
+			</View>
+		</View>
 	</SafeAreaView>		
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16
-  },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8
-  },
-  header: {
-    fontSize: 32,
-    backgroundColor: "#fff"
-  },
-  title: {
-    fontSize: 24
-  }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		marginHorizontal: 16,
+	},
+	fixToText: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	separator: {
+		marginVertical: 8,
+		borderBottomColor: '#737373',
+		borderBottomWidth: StyleSheet.hairlineWidth,
+	},
 });
+	
 
 
 export default Test;
