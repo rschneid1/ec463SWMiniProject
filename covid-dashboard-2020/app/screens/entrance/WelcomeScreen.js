4372c518bcd059c75631f2ce5cc362127c83b710
sshahid-bu <sshahid@bu.edu>
2020-09-17 14:58:15 -0400

    backend: firebase auth working initial

--- covid-dashboard-2020/app/screens/entrance/WelcomeScreen.js
+++ covid-dashboard-2020/app/screens/entrance/WelcomeScreen.js
@@ -1,82 +1,80 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableHighlight, TouchableOpacity } 
-  from 'react-native';
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, TouchableOpacity } 
+        from 'react-native';
 import background from '../../assets/abstract_background.jpg'
 
-const imgW = 360;
-const imgH = 180;
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import { firebaseConfig } from '../../../config';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 
-class WelcomeScreen extends React.Component {
-  render() {
-    return (
-      <ImageBackground style={styles.background} resizeMode="cover" source={background}>
-        <SafeAreaView style={styles.container}>
-          <StatusBar style="auto" />
+// Initialize Firebase
+if(!firebase.apps.length) { firebase.initializeApp(firebaseConfig); }
 
-          <TouchableHighlight> 
-            <Image source={{ width:imgW, height:imgH, uri:`https://picsum.photos/${imgW}/${imgH}` }}/>
-          </TouchableHighlight>
-          
-          <Text style={styles.welcome}> Welcome to </Text>
-          <Text style={styles.appName}> COVID-19 Daily Tracker </Text>
-          
-          <TouchableOpacity onPress={() => this.props.navigation.navigate('Login') } style={styles.loginButton}>
-            <Text style={styles.buttonText}> Login with Google </Text>
-          </TouchableOpacity>
+// https://docs.expo.io/guides/authentication/#google
+WebBrowser.maybeCompleteAuthSession();
 
-        </SafeAreaView>
-      </ImageBackground>
-    )  
-  }
+export default function WelcomeScreen( {navigation} ) {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+      clientId: '467420770701-jvmc15drmmrh1ng1kn4vnl1v3943spma.apps.googleusercontent.com',
+      },
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+      
+      const credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+      firebase.auth().signInWithCredential(credential);
+      navigation.navigate('Dashboard');
+    }
+  }, [response]);
+
+  const imgW = 360;
+  const imgH = 180;
+
+  return (
+    <ImageBackground style={styles.background} resizeMode="cover" source={background}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+
+        <Image source={{width:imgW, height:imgH, uri:`https://picsum.photos/${imgW}/${imgH}`}}/>
+        
+        <Text style={styles.welcome}> Welcome to </Text>
+        <Text style={styles.appName}> COVID-19 Daily Dashboard </Text>
+        
+        <TouchableOpacity onPress={() => { promptAsync(); } } style={styles.loginButton}>
+          <Text style={styles.buttonText}> Login with Google </Text>
+        </TouchableOpacity>
+
+      </SafeAreaView>
+    </ImageBackground>
+  )
 }
 
-export default WelcomeScreen;
-
 const styles = StyleSheet.create({
-  container: { 
-    flex:1, 
-    alignItems:'center', 
-    justifyContent:'center',
+  container: 
+  { flex:1, alignItems:'center', justifyContent:'center',
   },
-  welcome: { 
-    color:'#222', 
-    fontSize:24, 
-    marginHorizontal:10, 
-    marginBottom:5,
-    marginTop:24,
+  welcome: 
+  { color:'#222', fontSize:24, marginHorizontal:10, marginBottom:5, marginTop:24,
   },
-  appName: { 
-    color:'#222', 
-    fontSize:32, 
-    marginHorizontal:10, 
-    marginBottom:10,
-    marginTop:0,
-    fontFamily:'Trebuchet MS',
+  appName: 
+  { color:'#222', fontSize:32, marginHorizontal:10, marginBottom:10, marginTop:0, fontFamily:'Trebuchet MS',
   },
-  loginButton: { 
-    backgroundColor:'dodgerblue', 
-    padding:6, 
-    borderRadius:5,
-    marginTop:10, 
-    width:"24%",
-    alignItems:'center',
+  loginButton: 
+  { backgroundColor:'dodgerblue', padding:6, borderRadius:5, marginTop:10, width:"24%", alignItems:'center',
   },
-  signupButton: {
-    backgroundColor:'darkred', 
-    padding:6, 
-    borderRadius:5, 
-    marginTop:4,
-    width:"24%",
-    alignItems:'center',
+  signupButton: 
+  { backgroundColor:'darkred', padding:6, borderRadius:5, marginTop:4, width:"24%", alignItems:'center',
   },
-  buttonText: { 
-    fontSize:20, 
-    color:'#FFF',
+  buttonText: 
+  { fontSize:20, color:'#FFF', 
   },
-  background: {
-    flex:1,
-    width:'100%',
-    height:'100%',
+  background: 
+  { flex:1, width:'100%', height:'100%',
   },
+
 })
